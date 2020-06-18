6b22a9fe32eec0ac7b73d2a1c8f4e234d595cdc2
StellaYangF <1062878468@qq.com>
2020-06-18 14:57:50 +0800

    feat(client): init client project

---
+++ client/src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.less';
+import './styles.less';
+
+ReactDOM.render((
+    <h1>Hello</h1>
+), document.getElementById('root'));
+
+console.log(1);
